50d465cd0c0f51b67879e2551efcc91607564cca
Cristiananc <cristiananc@hotmail.com>
2020-05-20 17:30:46 -0300

    search function

---
+++ Search.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+//Clean the user-input considering the alphabet used in the trie
+//split words if necessary and then do the search in the tree for each word
+vector<string> clean_input(string words){
+    for (int i= 0; i < words.length(); i++){
+        string word_clean;
+        //I have to check if the char is in the alphabet, if it's not I exclude it
+        if ()
+        if is_number(words[i]){
+            word_clean.push_back(words[i]);
+        }
+        ((int) words[i]  
+    }
+
+
+}
+
+	bool is_wordchar(char c){
+	    return (('a' <= c && c <= 'z') or ('0' <= c && c <= '9'));
+	}
+
+//Return titles sorted
+//Return texts
+
+
+//Return true if a char is a digit
+bool is_number(string s){
+    return ((int) s => 48 && (int) s <= 57){
+}
+
+int main() {
+string wordsToSearch;
+cout << "What do you want to search?" << endl;
+getline(cin, wordsToSearch, '\n'); //Stores the whole sentence searched in a string variable
+
+}
