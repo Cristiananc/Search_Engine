c85eb5ae82970145d43f0ef56611b50521cf7cd1
Cristiananc <cristiananc@hotmail.com>
2020-05-25 23:26:08 -0300

    Searching +one word and displaying titles

--- Search.cpp
+++ Search.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <fstream>
+#include <fstream> //files
+#include <sstream>
+#include <ctime>
 using namespace std;
 
 const int ALPHABET_SIZE = 36;
@@ -17,12 +19,12 @@ struct Node{
 	}
 };
 
+
 class Trie{
 
 public:
     Node *pRoot = new Node;
     Trie(){}
-
     //Checks if a char belongs to the alphabet being used.
     bool is_alphabetchar(char c){
 	    return (('a' <= c && c <= 'z') or ('0' <= c && c <= '9'));
@@ -41,6 +43,66 @@ public:
 	    }
     }
 
+    void insert(string word, int docId){
+		Node *p = pRoot;
+		Node *pChar;
+		for (int i = 0; i < word.length() ; i++){
+		    int ind = index(word[i]);
+    		if(p -> children[ind] == nullptr){
+    		    pChar = new Node();
+    		    p -> children[ind] = pChar;
+    		}
+    	    p = p -> children[ind];
+		}
+		auto it = equal_range((p->documents).begin(), (p->documents).end(), docId);
+		if (it.first == it.second){(p->documents).insert(it.first, docId);}
+	}
+
+void leitura(string texto) {
+        clock_t t0 = clock();
+        bool f;             // verifica se faz parte de um id
+        int i = 0;          // conta as palavras
+        int id;
+        string word ;       // a palavra de cada iteraçao
+        char c ;            // a letra em cada iteração
+        char c2;            // a letra da iteração anterior
+        ifstream arquivo;   // arquivo a ser lido
+
+        arquivo.open(texto);
+        if(arquivo.is_open()){
+
+            while(arquivo.get(c)) {
+
+                if (c == ' ' or c == '\n'){ // reconhecendo término de uma palavra
+                    if(c == ' ' && c2 == '\n') { f = true;
+                    }
+                    if (f && word[0] == 'i' && word[1] == 'd' && word[2] == '=' && isdigit(word[3]) ){
+                        id = stoi(word.substr(3));
+                        word = "";
+                        f = false;
+                        i ++;
+                    }
+                    else if(word == ""){
+                    }
+                    else {
+                        insert(word, id); // chamando a função de insertar
+                        word = ""; // reinicio como vazia a variável word
+                        i ++;
+                        f = false;
+                    }
+                }
+                else {
+                    word = word+c; // acrescento à palavra o char atual
+                }
+                c2 = c;
+            }
+        }
+        arquivo.close();
+        //cout << ((((((pRoot)->children[23])->children[28])->children[29])->children[23])->documents)[2l] << endl;
+
+        clock_t tf = ((float)(clock()-t0))/CLOCKS_PER_SEC; // calculando tempo em segundos
+        cout << "segundos: "  << tf << endl << "palavras: " << i << endl;
+    }
     //Clean the user-input considering the alphabet used in the trie and split words if necessary 
     vector<string> clean_input(string words){
     vector<string> wordsToSearch;
@@ -55,28 +117,20 @@ public:
 
     for (int j= 0; j < words.length(); j++){
         //I have to check if the char is in the alphabet, if so 
-        if (is_alphabetchar(words[j])){
-            word_clean.push_back(words[j]);
+        if (is_alphabetchar(words[j])){word_clean.push_back(words[j]);
         }
-        else if(is_uppercase(words[j])){
-            word_clean.push_back(tolower(words[j]));
+        else if(is_uppercase(words[j])){word_clean.push_back(tolower(words[j]));
         }
-        else if (find(a.begin(), a.end(),(int) words[j]) != a.end()){
-            word_clean.push_back('a');}
-        else if (find(e.begin(), e.end(),(int) words[j]) != e.end()){
-            word_clean.push_back('e');
+        else if (find(a.begin(), a.end(),(int) words[j]) != a.end()){word_clean.push_back('a');}
+        else if (find(e.begin(), e.end(),(int) words[j]) != e.end()){word_clean.push_back('e');
         }
-        else if (find(i.begin(), i.end(),(int) words[j]) != i.end()){
-            word_clean.push_back('i');
+        else if (find(i.begin(), i.end(),(int) words[j]) != i.end()){word_clean.push_back('i');
         }
-        else if(find(o.begin(), o.end(),(int) words[j]) != o.end()){
-            word_clean.push_back('o');
+        else if(find(o.begin(), o.end(),(int) words[j]) != o.end()){word_clean.push_back('o');
         }
-        else if(find(u.begin(), u.end(),(int) words[j]) != u.end()){
-            word_clean.push_back('u');
+        else if(find(u.begin(), u.end(),(int) words[j]) != u.end()){word_clean.push_back('u');
         }
-        else if(find(c.begin(), c.end(),(int) words[j]) != c.end()){
-            word_clean.push_back('c');
+        else if(find(c.begin(), c.end(),(int) words[j]) != c.end()){word_clean.push_back('c');
         }
         else if(words[j] == ' '){
             wordsToSearch.push_back(word_clean);
@@ -89,52 +143,81 @@ public:
     return wordsToSearch;
 }
 
-    //Searching for a word in the tree
+//Searching for a word in the tree
     vector<int> search(string key){
     Node *pCurr = pRoot;
     for(int i =0; i < key.length(); i++){
         int ind = index(key[i]);
         if(!pCurr -> children[ind]){
-            return (pCurr -> documents);
+            vector<int> vec;
+            return vec;
         }
         pCurr = pCurr -> children[ind];
     }
     return (pCurr -> documents);
     }
 
-//Do the search in the tree for each word returned from the clean-input function
-    void searching(string word){
-        vector<string> words;
-        words = clean_input(word);
-        for(int i=0; i < words.size(); i++){
-            vector<int> ids;
-            ids = search(words[i]);
-            getTitle(ids);
+    	void serializacao(string name){
+		ofstream file;
+		file.open(name);
+		Node * pNode = pRoot;
+		exec_serializacao(pNode, file);
+	}
+
+	void exec_serializacao(Node * pCur, ofstream & file){
+		for(int i = 0 ; i < ALPHABET_SIZE ; i++ ){
+			if (pCur -> children[i] != nullptr){
+				file << i ;
+				for(int j = 0 ; j < (pCur->documents).size(); j++ ){// nao ta escrevendo no file
+					file << " " << (pCur -> documents)[j] << " ";
+				}
+				exec_serializacao(pCur-> children[i], file);
+			}
+		}
+		file << "]";
+	}
+
+	void disserializacao(string name){
+		ifstream file;
+		string line;
+        file.open(name);
+        getline(file, line);
+        Node ** pNode = &pRoot;
+        stringstream split;
+        split << line;
+        exec_pRoot_disserializacao(pNode, split);
+	}
+
+	void exec_pRoot_disserializacao(Node ** pNode, stringstream & split){
+        string cur_word;
+        while(split >> cur_word){
+            if(exec_disserializacao(pNode, cur_word, split)) break;
         }
     }
 
-    	void insert(string word, int docId){	
-		Node *p = pRoot;
-		Node *pChar;
-		for (int i = 0; i < word.length() ; i++){
-		    int ind = index(word[i]);
-    		if(p -> children[ind] == nullptr){
-    		    pChar = new Node();
-    		    p -> children[ind] = pChar;
-    		}
-    	    p = p -> children[ind];
-		}
-		//end of word
-		(p -> documents).push_back(docId);
-	}
+    bool exec_disserializacao(Node ** pNode, string cur_word, stringstream  & split){
+        if(cur_word == "]") return 1;
 
+        //Node * pNew = new Node(cur_word);
+        //(*pNode)->children[cur_word] = pNew;
+        //pNode = &(*pNode)->children[cur_word];
+
+        while(split >> cur_word){
+            if(exec_disserializacao(pNode, cur_word, split)) break;
+        }
+        return 0;
+    }
+
+//Return titles sorted
     void getTitle(vector<int> ids){
         ifstream titlesFile;
         string line;
         //Exemplo
         titlesFile.open("titulos.txt");
-        int countLIne = 0;
+        int countLine = 0;
         int indexVec = 0;
+        string aux;
+        int numberOfTitles = 1;
         if(!titlesFile){
             cerr << "Unable to open the file cointaning the page titles";
             exit(1); //call system to stop
@@ -142,41 +225,95 @@ public:
         else{
             while(getline(titlesFile, line) && indexVec < ids.size()){
                 int a = ids[indexVec];
-                if(countLIne == a){
+                if(countLine == a){
                     cout << line << endl;
                     ++ indexVec;
+
+                    if(indexVec == ids.size()-1) {cout << endl << endl <<  "encerrando pesquisa para a palavra:)" << endl << endl;}
+
+                    if(indexVec == numberOfTitles*20){cout << "tecle s + ENTER para a proxima pagina ou outra tecla + ENTER para sair: "; cin >> aux; if(aux != "s")
+                        {cout << endl << endl << "encerrando pesquisa :)" << endl;break;}
+                    else{numberOfTitles += 1;}
+                    }
                 }
-                ++ countLIne;
+                ++ countLine;
+            }
+        titlesFile.close();
+        }
+    }
+
+//Do the search in the tree for each word returned from the clean-input function
+    	void pesquisa(){
+	    vector<int> v  = {};
+	    Node *p = pRoot;
+        string word;
+        vector<string> words;
+        int auxx = 1;
+        cout << "O que procuras (aperte ENTER para pesquisar): " << endl;
+        getline(cin, word);
+        words = clean_input(word);
+
+        for(int i=0; i < words.size(); i++){
+            vector<int> ids;
+            ids = search(words[i]);
+            if (ids.size() == 0){
+                cout << "Desculpe, não encontramos sua pesquisa para a palavra " << words[i] << " :(" << endl;
+            }
+            else{
+                cout << "Pagínas encontradas para a palavra: " << words[i] << endl;
+                getTitle(ids);
             }
         }
-        titlesFile.close();
-    }
 
-    void exec(){
-        string wordsToSearch;
-        cout << "What do you want to search?" << endl;
-        getline(cin, wordsToSearch); //Stores the whole sentence searched in a string variable
-        searching(wordsToSearch);
-
-    }
-
-};
-
+	}
 
 //Intersecao de ids no caso de mais de uma palavra sendo pesquisada
-//Return titles sorted
 //Return texts
+
+};
 
 
 int main() {
 
 Trie Trie;
-int id1 = 4000;
-int id2 = 2900;
-int id3 = 3690;
-Trie.insert("the", id1);
-Trie.insert("games", id2);
-Trie.insert("hunger", id3);
 
-Trie.exec(); 
+Trie.serializacao("serializa��o");
+Trie.leitura("TEXTO_00.txt");
+Trie.pesquisa();
+//cout << (Trie.pRoot.children[10].children.[20].children[0].children[11].children[0].documents).size();
+
+return 0;
 } 
+
+
+/*for(int i = 0; i<palavra.length(); i++){
+                if ( i == palavra.length()-1){
+                    if( b && p != pRoot) { v = (p->documents); cout << (p->documents)[0] << " aqui "; b = false;}
+                    else if(palavra != "" && p != pRoot){
+                        int t1 = v[0];
+                        int t2 = (p->documents)[0];
+                        const int t3 = v[v.size()-1];
+                        const int t4 = (p->documents)[(p->documents).size()-1];
+                        while(t1 <= t3 && t2 <= t4) {
+                            if((p->documents)[c] != v[c] && (p->documents)[c] > v[c]){
+                                v.erase(v.begin()+c);
+                                if(t1 == t3){t1 = t1+100;}
+                                else {t1 = v[c];}
+                            }
+                            else if((p->documents)[c] != v[c] && (p->documents)[c] < v[c]){
+                                (p->documents).erase((p->documents).begin()+c);
+                                if(t2 == t4){t2 = t2+100;}
+                                else {t2 = (p->documents)[c];}
+                            }
+                            else if((p)->documents[c] == v[c]) { c = c+1;}
+                        }
+                        if ((p->documents).size() == 0) {v = {};}
+                    }
+                    p = pRoot;
+                }
+            else{
+                c = 0;
+                int ind = index(palavra[i]);
+                p = p->children[ind];
+            }
+        }*/
