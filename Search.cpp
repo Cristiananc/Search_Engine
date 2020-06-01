72c9cdaa4f2239cf8793165db009003885a8eb52
Cristiananc <cristiananc@hotmail.com>
2020-06-01 01:40:00 -0300

    Intersection of vector ids

--- Search.cpp
+++ Search.cpp
@@ -148,18 +148,14 @@ void leitura(string texto) {
 
 //Searching for a word in the tree
     vector<int> search(string key){
-    clock_t t0 = clock();
     Node *pCurr = pRoot;
     for(int i =0; i < key.length(); i++){
         int ind = index(key[i]);
         if(!pCurr -> children[ind]){
-            vector<int> vec;
-            return vec;
+            return (pCurr -> documents);
         }
         pCurr = pCurr -> children[ind];
     }
-    double tf = ((double)(clock()-t0))/(CLOCKS_PER_SEC/1000); // calculando tempo em segundos
-    cout << "(" << tf << " segundos)" << endl;
     return (pCurr -> documents);
     }
 
@@ -238,7 +234,7 @@ void leitura(string texto) {
                     if(indexVec == ids.size()) {cout << endl << endl <<  "encerrando pesquisa para a palavra :)" << endl << endl;}
 
                     if(indexVec == numberOfTitles*20){cout << "tecle s + ENTER para a proxima pagina ou outra tecla + ENTER para sair: "; cin >> aux; if(aux != "s")
-                        {cout << endl << endl << "encerrando pesquisa :)" << endl;break;}
+                        {cout << endl << endl << "encerrando pesquisa :)" << endl; break;}
                     else{numberOfTitles += 1;}
                     }
                 }
@@ -250,30 +246,62 @@ void leitura(string texto) {
 
 //Do the search in the tree for each word returned from the clean-input function
     	void pesquisa(){
-	    vector<int> v  = {};
 	    Node *p = pRoot;
         string word;
         vector<string> words;
         int auxx = 1;
+
+        while(true){
         cout << "O que procuras (aperte ENTER para pesquisar): " << endl;
         getline(cin, word);
         words = clean_input(word);
         vector<int> ids;
+        vector<int> ids2;
 
-        for(int i=0; i < words.size(); i++){
-            ids = search(words[i]);
+        clock_t t0 = clock();
+
+        ids = search(words[0]);
+        for(int i = 1; i < words.size(); i++){
+                ids2 = search(words[i]);
+                ids = intersection(ids, ids2);
+            }
+        
+        double tf = ((double)(clock()-t0))/(CLOCKS_PER_SEC/1000); // calculando tempo em segundos
+        cout << "(" << tf << " segundos)" << endl;
+
             cout << "Foram encontrados " << ids.size() << " resultados para sua pesquisa!" << endl;
             if (ids.size() == 0){
-                cout << "Desculpe, não encontramos sua pesquisa para a palavra " << words[i] << " :(" << endl;
+                cout << "Desculpe, não encontramos sua pesquisa para " << word << " :(" << endl;
             }
             else{
-                cout << "Pagínas encontradas para a palavra: " << words[i] << endl;
+                cout << "Pagínas encontradas para: " << word << endl;
                 getTitle(ids);
             }
+
+            cin.ignore();
         }
 	}
 
 //Intersecao de ids no caso de mais de uma palavra sendo pesquisada
+vector<int> intersection(vector<int> ids1, vector<int> ids2){
+    vector<int> idsIntersec = {};
+    int i = 0;
+    int j = 0;
+    while(i < ids1.size() && j < ids2.size()){
+    if(ids1[i] == ids2[j]){
+        idsIntersec.push_back(ids1[i]);
+        i ++;
+        j ++;
+    }
+    else if(ids1[i] < ids2[j]){
+        i ++;
+    }
+    else{
+        j ++;
+    }
+}
+return idsIntersec;
+}
 //Sugestão de palavras
 //Return texts
 
@@ -285,10 +313,9 @@ int main() {
 Trie Trie;
 
     Trie.serializacao("serializa��o");
-    cout << (int) 'í' << endl;
 
-    DIR *  dir;
-    struct dirent *entry;
+    DIR* dir;
+    struct dirent* entry;
     dir  = opendir("out_rept");
     while((entry = readdir(dir))){
             string s = entry->d_name;
